e8a32c72c9a2931d99281602fd8b53e2a85d1a35
fauzan.arkademi <fauzan.arkademi@gmail.com>
2023-06-07 15:51:40 +0700

    update gradio

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def send_message(channel, message, thread_ts=None):
         print("Error sending message to Slack:", e)        
 
 def send_to_chatbase(messages):
-    client = Client("https://dae84c56ad95f0fda6.gradio.live")
+    client = Client("https://c752aa13389f89c3d9.gradio.live")
     result = client.predict(
 				messages,	# str representing input in 'Enter your text' Textbox component
 				api_name="/predict"
